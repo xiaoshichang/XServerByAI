dc854ff4b848036b8c68cf4cee12abd15922847c
xiaoshichang <511680777@qq.com>
2026-03-12 22:33:50 +0800

    developer: M1-01 add top-level CMake placeholders

---
+++ cmake/toolchain-linux.cmake
@@ -0,0 +1,9 @@
+﻿# Placeholder Linux toolchain file.
+# Usage:
+#   cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-linux.cmake
+
+if(WIN32)
+  message(WARNING "Using Linux toolchain file on Windows host.")
+endif()
+
+set(XS_TOOLCHAIN "linux")
---
+++ cmake/toolchain-windows.cmake
@@ -0,0 +1,9 @@
+﻿# Placeholder Windows toolchain file.
+# Usage:
+#   cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-windows.cmake
+
+if(NOT WIN32)
+  message(WARNING "Using Windows toolchain file on non-Windows host.")
+endif()
+
+set(XS_TOOLCHAIN "windows")
