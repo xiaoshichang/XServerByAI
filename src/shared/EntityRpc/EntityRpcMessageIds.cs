537fb1538fdba8284a2ec929d82dbd74d22fcd01
xiaoshichang <511680777@qq.com>
2026-04-12 18:16:12 +0800

    developer: link shared entity rpc sources into framework rpc views

--- src/shared/EntityRpc/EntityRpcMessageIds.cs
+++ src/shared/EntityRpc/EntityRpcMessageIds.cs
@@ -1,6 +1,6 @@
 #if XSERVER_CLIENT_FRAMEWORK
 namespace XServer.Client.Rpc;
-#elif XSERVER_MANAGED_FRAMEWORK
+#elif XSERVER_SERVER_FRAMEWORK
 namespace XServer.Managed.Framework.Rpc;
 #else
 #error EntityRpc shared sources must be compiled by a framework project.
